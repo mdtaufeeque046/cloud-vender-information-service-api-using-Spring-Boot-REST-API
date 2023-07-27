8388aba4043667a0b25de7cbbdc90d7df420b346
mdtaufeeque046 <md7870taufeequea@gmail.com>
2023-07-27 16:37:36 +0530

    Initial commit

---
+++ src/test/java/com/taufeeque/cloudvenderinformationserviceapi/CloudVenderInformationServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taufeeque.cloudvenderinformationserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudVenderInformationServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
