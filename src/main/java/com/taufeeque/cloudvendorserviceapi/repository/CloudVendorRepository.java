ba2f1f427eda85ea59f732f16b847635fd37ac5c
Md Taufeeque Alam <md7870taufeequea@gmail.com>
2023-08-09 23:15:21 +0530

    Tested basic codes

--- src/main/java/com/taufeeque/cloudvendorserviceapi/repository/CloudVendorRepository.java
+++ src/main/java/com/taufeeque/cloudvendorserviceapi/repository/CloudVendorRepository.java
@@ -4,6 +4,9 @@ import com.taufeeque.cloudvendorserviceapi.model.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+    List<CloudVendor> findByVendorName(String vendorName);
 }
---
+++ src/test/java/com/taufeeque/cloudvendorserviceapi/repository/CloudVendorRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.taufeeque.cloudvendorserviceapi.repository;
+
+import com.taufeeque.cloudvendorserviceapi.model.CloudVendor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class CloudVendorRepositoryTest {
+
+    @Autowired
+    private CloudVendorRepository cloudVendorRepository;
+    CloudVendor cloudVendor;
+
+    @BeforeEach
+    void setUp() {
+        cloudVendor = new CloudVendor("1", "Amazon", "Bihar", "7667xxxx12");
+        cloudVendorRepository.save(cloudVendor);
+    }
+
+    @AfterEach
+    void tearDown() {
+        cloudVendor = null;
+        cloudVendorRepository.deleteAll();
+    }
+
+    //Test case SUCCESS
+    @Test
+    void testFindByVendorName_Found() {
+        List<CloudVendor> cloudVendorList = cloudVendorRepository.findByVendorName("Amazon");
+        assertThat(cloudVendorList.get(0).getVendorId()).isEqualTo(cloudVendor.getVendorId());
+        assertThat(cloudVendorList.get(0).getVendorAddress()).isEqualTo(cloudVendor.getVendorAddress());
+
+    }
+
+    //Test case FAILURE
+}
